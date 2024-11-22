af4ade3727843ddfbde5eda8cbc26b1f2b4568c0
Denis666-ctrl <ddneprovskij02@gmail.com>
2024-11-22 23:34:52 +0900

    Initial commit

---
+++ library_project/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using git_library;
+namespace git_library_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 func = new Class1();
+            func.HelloWorld();
+
+            func.AddAndPrint(3, 4);
+
+            string greeting = func.GetGreeting("Денис");
+            Console.WriteLine(greeting);
+
+            int sum = func.AddNumbers(5, 20);
+            Console.WriteLine($"Сумма: {sum}");
+
+            bool isEven = func.IsEven(14);
+            Console.WriteLine($"Число 14 четное? {isEven}");
+        }
+    }
+}
